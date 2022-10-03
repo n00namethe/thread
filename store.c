5d87ca39dcedc9343aedab017e3a2f9d84767ae6
n00namethe <n00namethe@gmail.com>
2022-10-04 00:59:19 +0700

    Убрал все brake и возможность небезопасной работы с критической областью. Дестрой происходит с ошибкой(анлок мьютекса не произошел)

--- store.c
+++ store.c
@@ -3,41 +3,45 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define COUNT_THREAD 3
+#define NUM_OF_THREAD 3
 #define SHOP_SIZE 1000
 #define LOAD_SIZE 1000
-#define CONST_FIVE 5
-#define CONST_THREE 3
+#define NUM_OF_MASSIV_SHOP 5
 #define COUNT_STORE 10000
-pthread_mutex_t mutex[CONST_FIVE] = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_mutex_t mutex[NUM_OF_MASSIV_SHOP];
 pthread_mutex_t mutex_loader = PTHREAD_MUTEX_INITIALIZER;
 
-int shop[CONST_FIVE] = {COUNT_STORE, COUNT_STORE, COUNT_STORE, COUNT_STORE, COUNT_STORE};
+int shop[NUM_OF_MASSIV_SHOP] = {COUNT_STORE, COUNT_STORE, COUNT_STORE, COUNT_STORE, COUNT_STORE};
 
 void* func(void*arg)
 {
     int *i = (int*)arg;
     printf("Я поток № %d зашел в func\n", *i);
-    size_t j; 
-    for (j = 0; j < CONST_FIVE; j++)
+    for (int j = 0; j < NUM_OF_MASSIV_SHOP; j++)
     {
+        if (pthread_mutex_init(&mutex[j], NULL) != 0)
+        {
+            perror("mutex don't init\n");
+        }
         if (pthread_mutex_trylock(&mutex[j]) == 0)
         {
-            if (shop[j] <= 0)
+            if (shop[j] - SHOP_SIZE < 0)
             {
-                printf("sold out dates. shop[%ld] = %d\n", j, shop[j]);
+                printf("sold out dates. shop[%d] = %d\n", j, shop[j]);
             }
             else
             {
                 shop[j] -= SHOP_SIZE;
-                printf("thread №%d.\t shop[%ld]\t = \t%d\n", *i, j, shop[j]);
+                printf("thread №%d.\t shop[%d]\t = \t%d\n", *i, j, shop[j]);
                 sleep(1);
             }
             pthread_mutex_unlock(&mutex[j]);
         } 
         else
         {
-            printf("no store available for buyer, next..\n");
+            printf("this shop[%d] don't available for buyer, go next..\n", j);
+            usleep(300000);
         }
     }
     return NULL;
@@ -45,57 +49,63 @@ void* func(void*arg)
 
 void* loader()
 {
-    size_t j;
-    for (j = 0; j < CONST_FIVE; j++)
+    for (int j = 0; j < NUM_OF_MASSIV_SHOP; j++)
     { 
         if (pthread_mutex_trylock(&mutex_loader) == 0)
         {
             if (shop[j] - LOAD_SIZE < 0)
             {
-                break;
+                printf("loader stop\n");
             }
             else
             {
                 shop[j] += LOAD_SIZE;
                 sleep(2);
-                printf("shop[%ld] + load = %d\n", j, shop[j]);
+                printf("shop[%d] + load = %d\n", j, shop[j]);
             }
             pthread_mutex_unlock(&mutex_loader);
         }
         else
         {
-            printf("no store available for loader, next try\n");
+            printf("this shop[%d] don't available for loader, go next\n", j);
         }
     }
-    return 0;
+    return NULL;
 }
 
 int main()
 {
-    size_t i;
-    pthread_t buyer[COUNT_THREAD];
+    int i;
+    pthread_t buyer[NUM_OF_THREAD];
     pthread_t load;
-    int index[COUNT_THREAD];
-    while(shop[i] > 0)
+    int*index;
+    int a;
+    index = &a;
+    for (i = 0; i < NUM_OF_THREAD; i++)
     {
-        for ( i = 0; i < CONST_THREE; i++)
+        *index = i + 1;
+        if (pthread_create(&buyer[i], NULL, &func, index) != 0)
         {
-            index[i] = i + 1;
-            if (pthread_create(&buyer[i], NULL, &func, &index[i]) != 0)
-            {
-                printf("Failed to create tread[%ld]\n", i);
-            }
+            printf("Failed to create thread[%d]\n", i);
         }
-        pthread_create(&load, NULL, loader, NULL);
-        for (i = 0; i < CONST_THREE; i++)
+    }
+    pthread_create(&load, NULL, loader, NULL);
+    for (i = 0; i < NUM_OF_THREAD; i++)
+    {
+        if (pthread_join(buyer[i], NULL) != 0)
         {
-            if (pthread_join(buyer[i], NULL) != 0)
-            {
-                printf("Failed to join thread[%ld]\n", i);
-            }
+            printf("Failed to join thread[%d]\n", i);
+        }
+    }
+    for (i = 0; i < NUM_OF_MASSIV_SHOP; i++)
+    {
+        int destroy;
+        destroy = pthread_mutex_destroy(&mutex[i]);
+        if (destroy != 0)
+        {
+            printf("pthread_mutex_destroy = %d\n", destroy);
         }
     }
     pthread_cancel(load);
-    pthread_mutex_destroy(&mutex[i]);
     return 0;
 }
